002b146bcc670dcd0da9da7568e5207f71c103e3
monks-commits <monks@mail.ru>
2025-12-12 00:05:07 +0200

    Update and rename index1.php to index_work.php

--- index_work.php
+++ index_work.php
@@ -1,7 +1,7 @@
 <!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.0 Transitional//EN">
-<html><head><title>Rock Cafe,  Rock Music, Днепропетровское "Рок-кафе"</title>
-<META NAME="keywords" CONTENT="rolling stones, Rolling Stones, rock music, hard rock, rock cafe, hardrockcafe, rock and roll, rock'n'roll, rock, rockcafe, jazz, pop music, hard rock, art rock, punk rock, heavy metal, country, soul, blues, disco, reggey, рок-кафе, рок, рок-музыка, музыка, рокер, рок-н-ролл, хард-рок, арт-рок, панк-рок, кантри, джаз, поп, диско, соул, блюз, регги">
-<META NAME="description" CONTENT="Рок-кафе, Днепропетровск - Long live rock'n roll!">
+<html><head><title>Rock Cafe,  Rock Music, Г„Г­ГҐГЇГ°Г®ГЇГҐГІГ°Г®ГўГ±ГЄГ®ГҐ "ГђГ®ГЄ-ГЄГ ГґГҐ"</title>
+<META NAME="keywords" CONTENT="rolling stones, Rolling Stones, rock music, hard rock, rock cafe, hardrockcafe, rock and roll, rock'n'roll, rock, rockcafe, jazz, pop music, hard rock, art rock, punk rock, heavy metal, country, soul, blues, disco, reggey, Г°Г®ГЄ-ГЄГ ГґГҐ, Г°Г®ГЄ, Г°Г®ГЄ-Г¬ГіГ§Г»ГЄГ , Г¬ГіГ§Г»ГЄГ , Г°Г®ГЄГҐГ°, Г°Г®ГЄ-Г­-Г°Г®Г«Г«, ГµГ Г°Г¤-Г°Г®ГЄ, Г Г°ГІ-Г°Г®ГЄ, ГЇГ Г­ГЄ-Г°Г®ГЄ, ГЄГ Г­ГІГ°ГЁ, Г¤Г¦Г Г§, ГЇГ®ГЇ, Г¤ГЁГ±ГЄГ®, Г±Г®ГіГ«, ГЎГ«ГѕГ§, Г°ГҐГЈГЈГЁ">
+<META NAME="description" CONTENT="ГђГ®ГЄ-ГЄГ ГґГҐ, Г„Г­ГҐГЇГ°Г®ГЇГҐГІГ°Г®ГўГ±ГЄ - Long live rock'n roll!">
 <META HTTP-EQUIV="content-type" CONTENT="text/html; charset=windows-1251">
 <META HTTP-EQUIV="cache-control" CONTENT="no-cache, must-revalidate">
 <META HTTP-EQUIV="pragma" CONTENT="no-cache">
@@ -48,7 +48,7 @@ window.open(URL, w, 'toolbar=0,scrollbars=0,location=0,statusbar=0,menubar=0,res
 <!-- 1-st string COLLAGE & BANNERS -->
 
 <tr>
-<td width=200 height=75 background="../img/guitar-top.gif"><img src="../img/zero.gif" alt="Рок-Кафе - Днепропетровск, Rock Music, Rock and Roll" width=200 height=75 border=0></td>
+<td width=200 height=75 background="../img/guitar-top.gif"><img src="../img/zero.gif" alt="ГђГ®ГЄ-ГЉГ ГґГҐ - Г„Г­ГҐГЇГ°Г®ГЇГҐГІГ°Г®ГўГ±ГЄ, Rock Music, Rock and Roll" width=200 height=75 border=0></td>
 <td width=100% height="75" background="../img/keyb1.gif" bgcolor=white><table 
 width=100% height="75" cellspacing=0 cellpadding=0 border=0>
 	<tr>
@@ -80,7 +80,7 @@ if(js>11)d.write('--'+'>')//--></script><!--/COUNTER--><a href="http://www.rock-
 
 	<table width=100% cellspacing=0 cellpadding=0 border=0>
 	<tr>
-	<td align=center width=200><a href=../index.php class=m title="Обложка сайта"><font color=#000000>www.rock-cafe.dp.ua</a></td>
+	<td align=center width=200><a href=../index.php class=m title="ГЋГЎГ«Г®Г¦ГЄГ  Г±Г Г©ГІГ "><font color=#000000>www.rock-cafe.dp.ua</a></td>
 	<td align=center>&nbsp;</td>
 	</tr>
 	</table>
@@ -88,7 +88,7 @@ if(js>11)d.write('--'+'>')//--></script><!--/COUNTER--><a href="http://www.rock-
 </td></tr>
 <tr><td width=100% height=1 colspan=2 bgcolor=black><img src="../img/zero.gif" alt="" width=1 height=1></td></tr>
 <tr><td width=100% height=1 colspan=2 bgcolor=#FFA500><img src="../img/zero.gif" alt="" width=1 height=1></td></tr>
-<!-- 2-nd string бегущая строка ---------------------------------- -->
+<!-- 2-nd string ГЎГҐГЈГіГ№Г Гї Г±ГІГ°Г®ГЄГ  ---------------------------------- -->
 
 <!-- <tr><td width=100% height= colspan="2" bgcolor="#FFA500"><img src="../img/zero.gif" alt="" width=100% height=1></td></tr> -->
 
@@ -107,52 +107,52 @@ if(js>11)d.write('--'+'>')//--></script><!--/COUNTER--><a href="http://www.rock-
 		<tr bgcolor='#000000'>
 <td width=1 bgcolor=white><img src="../img/zero.gif" alt="" width=1 height=1></td>
 <td bgcolor='#000000'
- onclick=cl(this) onmouseout=ou(this) onmouseover=ov(this)><a class=m href="../about.php">&nbsp;О нашем "Рок-Кафе"</a></td></tr>
+ onclick=cl(this) onmouseout=ou(this) onmouseover=ov(this)><a class=m href="../about.php">&nbsp;ГЋ Г­Г ГёГҐГ¬ "ГђГ®ГЄ-ГЉГ ГґГҐ"</a></td></tr>
 		<tr bgcolor='#000000'>
 <td width=1 bgcolor=white><img src="../img/zero.gif" alt="" width=1 height=1></td>
 <td bgcolor='#000000'
- onclick=cl(this) onmouseout=ou(this) onmouseover=ov(this)><a class=m href="../afisha.php">&nbsp;Афиша</a></td></tr>
+ onclick=cl(this) onmouseout=ou(this) onmouseover=ov(this)><a class=m href="../afisha.php">&nbsp;ГЂГґГЁГёГ </a></td></tr>
 		<tr bgcolor='#000000'>
 <td width=1 bgcolor=white><img src="../img/zero.gif" alt="" width=1 height=1></td>
 <td bgcolor='#000000'
- onclick=cl(this) onmouseout=ou(this) onmouseover=ov(this)><a class=m href="../news/news.php" target=_blank>&nbsp;Новости</a></td></tr>
+ onclick=cl(this) onmouseout=ou(this) onmouseover=ov(this)><a class=m href="../news/news.php" target=_blank>&nbsp;ГЌГ®ГўГ®Г±ГІГЁ</a></td></tr>
 		<tr bgcolor='#000000'>
 <td width=1 bgcolor=white><img src="../img/zero.gif" alt="" width=1 height=1></td>
 <td bgcolor='#000000'
- onclick=cl(this) onmouseout=ou(this) onmouseover=ov(this)><a class=m href="http://www.rock-cafe.dp.ua/seva/seva.php" target="_blank">&nbsp;"Рок-Посевы" B.B.C.</a> <font color=white face=verdana size=2><b>&nbsp;NEW&nbsp;</b></font></td></tr>
+ onclick=cl(this) onmouseout=ou(this) onmouseover=ov(this)><a class=m href="http://www.rock-cafe.dp.ua/seva/seva.php" target="_blank">&nbsp;"ГђГ®ГЄ-ГЏГ®Г±ГҐГўГ»" B.B.C.</a> <font color=white face=verdana size=2><b>&nbsp;NEW&nbsp;</b></font></td></tr>
 
 		<tr bgcolor='#000000'>
 <td width=1 bgcolor=white><img src="../img/zero.gif" alt="" width=1 height=1></td>
 <td bgcolor='#000000'
- onclick=cl(this) onmouseout=ou(this) onmouseover=ov(this)><a class=m href="../menu1.php">&nbsp;Кухня/Бар</a></td></tr>
+ onclick=cl(this) onmouseout=ou(this) onmouseover=ov(this)><a class=m href="../menu1.php">&nbsp;ГЉГіГµГ­Гї/ГЃГ Г°</a></td></tr>
 		<tr bgcolor='#000000'>
 <td width=1 bgcolor=white><img src="../img/zero.gif" alt="" width=1 height=1></td>
 <td bgcolor='#000000'
- onclick=cl(this) onmouseout=ou(this) onmouseover=ov(this)><a class=m href="../menu2.php"><font color=white><b>&nbsp;+&nbsp;</b></font>Музыкально Меню<font color=white><b>&nbsp;+&nbsp;</b></font></a></td></tr>
+ onclick=cl(this) onmouseout=ou(this) onmouseover=ov(this)><a class=m href="../menu2.php"><font color=white><b>&nbsp;+&nbsp;</b></font>ГЊГіГ§Г»ГЄГ Г«ГјГ­Г® ГЊГҐГ­Гѕ<font color=white><b>&nbsp;+&nbsp;</b></font></a></td></tr>
 		<tr bgcolor='#000000'>
 <td width=1 bgcolor=white><img src="../img/zero.gif" alt="" width=1 height=1></td>
 <td bgcolor='#000000'
- onclick=cl(this) onmouseout=ou(this) onmouseover=ov(this)><a class=m href="http://www.rock-cafe.dp.ua/pascal/history.php" target="_blank">&nbsp;История Рок-Музыки</a></td></tr>
+ onclick=cl(this) onmouseout=ou(this) onmouseover=ov(this)><a class=m href="http://www.rock-cafe.dp.ua/pascal/history.php" target="_blank">&nbsp;Г€Г±ГІГ®Г°ГЁГї ГђГ®ГЄ-ГЊГіГ§Г»ГЄГЁ</a></td></tr>
 		<tr bgcolor='#000000'>
 <td width=1 bgcolor=white><img src="../img/zero.gif" alt="" width=1 height=1></td>
 <td bgcolor='#000000'
- onclick=cl(this) onmouseout=ou(this) onmouseover=ov(this)><a class=m href="../photo.php">&nbsp;Фотогалерея</a></td></tr>
+ onclick=cl(this) onmouseout=ou(this) onmouseover=ov(this)><a class=m href="../photo.php">&nbsp;Г”Г®ГІГ®ГЈГ Г«ГҐГ°ГҐГї</a></td></tr>
 		<tr bgcolor='#000000'>
 <td width=1 bgcolor=white><img src="../img/zero.gif" alt="" width=1 height=1></td>
 <td bgcolor='#000000'
- onclick=cl(this) onmouseout=ou(this) onmouseover=ov(this)><a class=m href="../coord.php">&nbsp;Наши координаты</a></td></tr>
+ onclick=cl(this) onmouseout=ou(this) onmouseover=ov(this)><a class=m href="../coord.php">&nbsp;ГЌГ ГёГЁ ГЄГ®Г®Г°Г¤ГЁГ­Г ГІГ»</a></td></tr>
 		<tr bgcolor='#000000'>
 <td width=1 bgcolor=white><img src="../img/zero.gif" alt="" width=1 height=1></td>
 <td bgcolor='#000000'
- onclick=cl(this) onmouseout=ou(this) onmouseover=ov(this)><a class=m href="../links.php">&nbsp;Обмен ссылками</a></td></tr>
+ onclick=cl(this) onmouseout=ou(this) onmouseover=ov(this)><a class=m href="../links.php">&nbsp;ГЋГЎГ¬ГҐГ­ Г±Г±Г»Г«ГЄГ Г¬ГЁ</a></td></tr>
 		<tr bgcolor='#000000'>
 <td width=1 bgcolor=white><img src="../img/zero.gif" alt="" width=1 height=1></td>
 <td bgcolor='#000000'
- onclick=cl(this) onmouseout=ou(this) onmouseover=ov(this)><a class=m href="http://www.guestbook.ru/book.cgi?user=rockcafe&action=show" target=_blank>&nbsp;Гостевая книга</a></td></tr>
+ onclick=cl(this) onmouseout=ou(this) onmouseover=ov(this)><a class=m href="http://www.guestbook.ru/book.cgi?user=rockcafe&action=show" target=_blank>&nbsp;ГѓГ®Г±ГІГҐГўГ Гї ГЄГ­ГЁГЈГ </a></td></tr>
 		<tr bgcolor='#000000'>
 <td width=1 bgcolor=white><img src="../img/zero.gif" alt="" width=1 height=1></td>
 <td bgcolor='#000000'
- onclick=cl(this) onmouseout=ou(this) onmouseover=ov(this)><a class=m href="../search.php" >&nbsp;Поиск по сайту</a></td></tr>
+ onclick=cl(this) onmouseout=ou(this) onmouseover=ov(this)><a class=m href="../search.php" >&nbsp;ГЏГ®ГЁГ±ГЄ ГЇГ® Г±Г Г©ГІГі</a></td></tr>
 		<tr bgcolor='#000000'>
 <td width=1 height=1 bgcolor=orange colspan=2><img src="../img/zero.gif" alt="" width= height=1></td></tr>
 		</table>
@@ -168,7 +168,7 @@ if(js>11)d.write('--'+'>')//--></script><!--/COUNTER--><a href="http://www.rock-
 href="http://top.mail.ru/jump?from=523924"><img
 src="http://top.list.ru/counter?id=523924;t=47;l=1"
 border=0 height=31 width=88
-alt="Рейтинг@Mail.ru" style="filter: alpha(opacity=50), purple" onmouseover=this.filters.alpha.opacity=100 onmouseout=this.filters.alpha.opacity=50></a><!--/LOGO-->
+alt="ГђГҐГ©ГІГЁГ­ГЈ@Mail.ru" style="filter: alpha(opacity=50), purple" onmouseover=this.filters.alpha.opacity=100 onmouseout=this.filters.alpha.opacity=50></a><!--/LOGO-->
 
 
 
@@ -177,7 +177,7 @@ alt="Рейтинг@Mail.ru" style="filter: alpha(opacity=50), purple" onmouseover=this
 
 
 <a href="JavaScript:popUp2('http://www.rock-cafe.dp.ua/technical_rider.html','myWin');"><img src="http://www.rock-cafe.dp.ua/img/rock-cafe-rider.gif" alt="Rock Cafe
-Стационарный технический райдер клуба" width=100 height=100 hspace=20 vspace=4 align=right border=0></a>
+Г‘ГІГ Г¶ГЁГ®Г­Г Г°Г­Г»Г© ГІГҐГµГ­ГЁГ·ГҐГ±ГЄГЁГ© Г°Г Г©Г¤ГҐГ° ГЄГ«ГіГЎГ " width=100 height=100 hspace=20 vspace=4 align=right border=0></a>
 
 </p>
 
@@ -199,7 +199,7 @@ alt="Рейтинг@Mail.ru" style="filter: alpha(opacity=50), purple" onmouseover=this
 	<td width=2 bgcolor=black><img src="../img/zero.gif" alt="" width=2 height=1></td>
 	<td width=100% valign=top class=td1>
 <?
-/* если запуск из корня */
+/* ГҐГ±Г«ГЁ Г§Г ГЇГіГ±ГЄ ГЁГ§ ГЄГ®Г°Г­Гї */
 if (!$inc)
 	require("about-content.php");
 else
@@ -220,12 +220,12 @@ else
 <tr><td width=100% height=1 colspan=2 bgcolor=#FFA500><img src="../img/zero.gif" alt="" width=1 height=1></td></tr>
 <tr><td width=100% height=1 colspan=2 bgcolor=black><img src="../img/zero.gif" alt="" width=1 height=1></td></tr>
 <tr>
-<td bgcolor=#FFA500><div class=mono>"Рок-Кафе"<font color="maroon"> &copy; </font>2003-2008</div><img src="../img/zero.gif" alt="" width=1 height=1></td>
+<td bgcolor=#FFA500><div class=mono>"ГђГ®ГЄ-ГЉГ ГґГҐ"<font color="maroon"> &copy; </font>2003-2008</div><img src="../img/zero.gif" alt="" width=1 height=1></td>
 <td>
 	<table width=100% cellspacing=0 cellpadding=0 background="" bgcolor=orange border=0>
 	<tr>
 <td  height=20 align=left><div class=mono>Design & Support<font color="maroon"> A.Kuksin </font> ICQ 31622216</div></td>
-<td  height=20 align=right><div class=mono>"Рок-Кафе" - г.Днепропетровск, наб.Победы, 106-а, тел. +380(562) 68-33-70</div></td>
+<td  height=20 align=right><div class=mono>"ГђГ®ГЄ-ГЉГ ГґГҐ" - ГЈ.Г„Г­ГҐГЇГ°Г®ГЇГҐГІГ°Г®ГўГ±ГЄ, Г­Г ГЎ.ГЏГ®ГЎГҐГ¤Г», 106-Г , ГІГҐГ«. +380(562) 68-33-70</div></td>
 	</td>
 	</tr>
 	</table>
